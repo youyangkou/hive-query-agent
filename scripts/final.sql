ae0287c82ceb192c0c0f7d7b9dd627797edb0128
Gerry <youyang.kou@relx.com>
2022-08-19 16:11:32 +0800

    update final.sql add comment

--- scripts/final.sql
+++ scripts/final.sql
@@ -1,14 +1,13 @@
--- auto-generated definition
 create table hive_query_bean
 (
-    query_id      varchar(255)                         not null
+    query_id      varchar(255)                         not null  comment '查询id'
         primary key,
-    query_sql     varchar(255)                         null,
-    project       varchar(255)                         null,
-    tmp_table     varchar(255)                         null,
-    is_only_query tinyint(1) default 0                 null,
-    query_state   varchar(255)                         null,
-    log           varchar(255)                         null,
-    update_time   datetime   default CURRENT_TIMESTAMP null
+    query_sql     varchar(255)                         null comment '查询sql',
+    project       varchar(255)                         null comment '查询database',
+    tmp_table     varchar(255)                         null comment '生成的临时表',
+    is_only_query tinyint(1) default 0                 null comment '是否只是简单查询',
+    query_state   varchar(255)                         null comment '查询状态',
+    log           varchar(255)                         null comment '查询日志',
+    update_time   datetime   default CURRENT_TIMESTAMP null comment '更新时间'
 )
-    collate = utf8mb4_general_ci;
+ comment 'bdp查询引擎记录表'
